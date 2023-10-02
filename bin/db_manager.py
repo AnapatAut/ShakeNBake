a6a3c15c5d5ebf966e13b8b11020012a9bb0705e
Aut <anapat5937@gmail.com>
2023-10-02 10:20:48 +0700

    added comments to db_manager

--- bin/db_manager.py
+++ bin/db_manager.py
@@ -1,4 +1,9 @@
+# db_manager.py
+#
+# Database manager used to as a center to interface with the database
+#
 # Created by Anapat B., 27 Sep 2023
+
 import os
 import sqlite3
 from sqlite3 import Error
@@ -16,6 +21,7 @@ db_file = parent_dir + "/data/recipe_database.db3"
 print(db_file)
 
 
+# Establish connection with the database
 def create_connection():
     """
     Connect with database
@@ -30,6 +36,10 @@ def create_connection():
     return conn
 
 
+# Create element to be added to the database
+# "conn" is the connection to the database
+# "task" is elements to be added to the database
+# "table" is the name of the table to add the data to
 def create_task(conn, task, table):
     """
     Create a new task
@@ -50,6 +60,11 @@ def create_task(conn, task, table):
         print(e)
 
 
+# Query a search in the database
+# "conn" is the connection to the database
+# "table" is the name of the table to query the data from
+# "header" is the header of the column to query from
+# "element" is the element we want to find
 def db_query(conn, table, header, element):
     """
     Query data from database
