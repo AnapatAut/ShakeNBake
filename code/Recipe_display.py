0008cc9b45c516d436a6a809218e75cd9499698a
Sira <kongsirisira@gmail.com>
2023-10-19 17:52:01 +0700

    changed name to match coding standard

--- code/Recipe_display.py
+++ code/Recipe_display.py
@@ -1,92 +1,93 @@
+"""This file gereates a window that displays the recipe of each menu item"""
+
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout, QLabel
+import math
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QTableWidget, QTableWidgetItem, QLabel
 from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QFont
-import math
 
-class Ui_MainWindow(QMainWindow):
+
+class MainWindow(QMainWindow):
+    """Generates the window"""
     def __init__(self):
         super().__init__()
-        self.setupUi()
+        self.setup_ui()
 
-    def setupUi(self):
-        tableWidth = 920
-        tableHeight = 920
-        self.setGeometry(0, 0, tableWidth, tableHeight)
+    def setup_ui(self):
+        """Setups the UI like buttons and tables"""
+        table_width = 920
+        table_height = 680
+        self.setGeometry(0, 0, table_width, table_height)
         self.centralwidget = QWidget(self)
-        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setObjectName("centralwidget")
         self.setCentralWidget(self.centralwidget)
 
-        self.EditBtn = QPushButton(self.centralwidget)
-        self.EditBtn.setObjectName(u"EditBtn")
-        self.EditBtn.setGeometry(QRect(720, 620, 80, 30))
+        self.edit_btn = QPushButton(self.centralwidget)
+        self.edit_btn.setObjectName("EditBtn")
+        self.edit_btn.setGeometry(QRect(720, 620, 80, 30))
 
-        self.RemoveBtn = QPushButton(self.centralwidget)
-        self.RemoveBtn.setObjectName(u"RemoveBtn")
-        self.RemoveBtn.setGeometry(QRect(820, 620, 80, 30))
+        self.remove_btn = QPushButton(self.centralwidget)
+        self.remove_btn.setObjectName("RemoveBtn")
+        self.remove_btn.setGeometry(QRect(820, 620, 80, 30))
 
-        self.textLabel = QLabel(self.centralwidget)
-        self.textLabel.setGeometry(QRect(35, 10, 662, 51))
-        self.textLabel.setText("Recipe")
+        self.text_label = QLabel(self.centralwidget)
+        self.text_label.setGeometry(QRect(35, 10, 662, 51))
+        self.text_label.setText("Recipe")
 
-        # Create a table widget
-
-        colum_len = math.floor(tableWidth / 3)
-        self.IngredientWidget = QTableWidget(self.centralwidget)
-        self.IngredientWidget.setGeometry(QRect(35, 80, 662, 261))
-        self.IngredientWidget.setColumnCount(3)  # Set the number of columns
-        self.IngredientWidget.setHorizontalHeaderLabels(["Column 1", "Column 2", "Column 3"])  # Set column headers
-        self.IngredientWidget.setRowCount(20)  # Set the number of rows
-        self.IngredientWidget.setColumnWidth(0, colum_len)
+        colum_len = math.floor(table_width / 3)
+        self.ingredient_widget = QTableWidget(self.centralwidget)
+        self.ingredient_widget.setGeometry(QRect(35, 80, 662, 261))
+        self.ingredient_widget.setColumnCount(3)  # Set the number of columns
+        self.ingredient_widget.setHorizontalHeaderLabels(["Column 1", "Column 2", "Column 3"])
+        self.ingredient_widget.setRowCount(20)  # Set the number of rows
+        self.ingredient_widget.setColumnWidth(0, colum_len)
 
         for row in range(5):
             ingredient = QTableWidgetItem(f"Ingredient {row + 1}")
-            self.IngredientWidget.setItem(row, 0, ingredient)
+            self.ingredient_widget.setItem(row, 0, ingredient)
             ingredient = QTableWidgetItem(f"Name {row + 1}")
-            self.IngredientWidget.setItem(row, 1, ingredient)
+            self.ingredient_widget.setItem(row, 1, ingredient)
             ingredient = QTableWidgetItem(f"Amount {row + 1}")
-            self.IngredientWidget.setItem(row, 2, ingredient)
-        
-        self.tableWidget = QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QRect(35, 370, 662, 261))
-        self.tableWidget.setColumnCount(3)  # Set the number of columns
-        self.tableWidget.setHorizontalHeaderLabels(["Column 1", "Column 2", "Column 3"])  # Set column headers
-        self.tableWidget.setRowCount(20)  # Set the number of rows
-        self.tableWidget.setColumnWidth(0, colum_len)
+            self.ingredient_widget.setItem(row, 2, ingredient)
+
+
+        self.table_widget = QTableWidget(self.centralwidget)
+        self.table_widget.setGeometry(QRect(35, 370, 662, 261))
+        self.table_widget.setColumnCount(3)
+        self.table_widget.setHorizontalHeaderLabels(["Steps:", "Column 2", "Column 3"])
+        self.table_widget.setRowCount(20)
+        self.table_widget.setColumnWidth(0, colum_len)
 
         for row in range(5):
             ingredient = QTableWidgetItem(f"Step {row + 1}")
-            self.IngredientWidget.setItem(row, 0, ingredient)
+            self.table_widget.setItem(row, 0, ingredient)
             ingredient = QTableWidgetItem(f"Name {row + 1}")
-            self.IngredientWidget.setItem(row, 1, ingredient)
+            self.table_widget.setItem(row, 1, ingredient)
             ingredient = QTableWidgetItem(f"Amount {row + 1}")
-            self.IngredientWidget.setItem(row, 2, ingredient)
+            self.table_widget.setItem(row, 2, ingredient)
 
-        
         font = QFont()
         font.setPointSize(20)  # Set the font size to 14
         font.setBold(True)  # Make the text bold
-        self.textLabel.setFont(font)
+        self.text_label.setFont(font)
 
 
-        for row in range(5):
-            ingredient = QTableWidgetItem(f"Ingredient {row + 1}")
-            self.IngredientWidget.setItem(row, 0, ingredient)
-            ingredient = QTableWidgetItem(f"Name {row + 1}")
-            self.IngredientWidget.setItem(row, 1, ingredient)
-            ingredient = QTableWidgetItem(f"Amount {row + 1}")
-            self.IngredientWidget.setItem(row, 2, ingredient)
+        self.retranslate_ui()
 
-        self.retranslateUi()
 
-    def retranslateUi(self):
+
+
+
+    def retranslate_ui(self):
+        """Set the names of title and buttons"""
         self.setWindowTitle("Recipe")
-        self.EditBtn.setText("Edit")
-        self.RemoveBtn.setText("Remove")
+        self.edit_btn.setText("Edit")
+        self.remove_btn.setText("Remove")
 
 def main():
+    """Runs the main function"""
     app = QApplication(sys.argv)
-    window = Ui_MainWindow()
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
 
