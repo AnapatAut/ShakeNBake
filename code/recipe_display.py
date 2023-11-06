0f14821b991b7eeeaa5eb81fe7ef55cedd97f8c1
Sira <kongsirisira@gmail.com>
2023-11-06 11:21:11 +0700

    fiixed some intergration problems and cleaned up the code

--- code/recipe_display.py
+++ code/recipe_display.py
@@ -1,9 +1,5 @@
 """This file gereates a window that displays the recipe of each menu item"""
 
-
-import sqlite3
-import datetime
-
 import sys
 import math
 from PyQt5.QtWidgets import QApplication
@@ -13,7 +9,6 @@ from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QTableWidget
 from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QStyledItemDelegate
 from PyQt5.QtCore import QRect, Qt
 from PyQt5.QtGui import QFont
 import db_manager as db
@@ -23,10 +18,11 @@ import db_manager as db
 
 class MainWindow(QMainWindow):
 
-    recipe_id = 2
     """Generates the window"""
     def __init__(self):
         super().__init__()
+        self.recipe_id = 1
+        self.main_data = []
         self.setup_ui()
 
     def setup_ui(self):
@@ -36,6 +32,7 @@ class MainWindow(QMainWindow):
         window_width = 920
         window_height = 680
 
+
         # Set the Window title (top left)
         self.setWindowTitle("Recipe Display")
 
@@ -44,73 +41,50 @@ class MainWindow(QMainWindow):
         self.centralwidget = QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.setCentralWidget(self.centralwidget)
-        
 
         # The edit button
-        self.edit_btn = QPushButton(self.centralwidget)
-        self.edit_btn.setObjectName("EditBtn")
-        self.edit_btn.setGeometry(QRect(720, 620, 80, 30))
-        self.edit_btn.setText("Edit")
+        self.remove_btn = QPushButton(self.centralwidget)
+        self.remove_btn.setObjectName("remove_tn")
+        self.remove_btn.setGeometry(QRect(820, 620, 80, 30))
+        self.remove_btn.setText("Remove")
 
 
         # The remove button
-        location = "recipe_display"
-        self.remove_btn = QPushButton(self.centralwidget)
-        self.remove_btn.setObjectName("push")
-        self.remove_btn.setGeometry(QRect(820, 620, 80, 30))
-        self.remove_btn.setText("Remove")       
-        # self.remove_btn.clicked.connect(self.remove_from_step_list)
+        self.back_btn = QPushButton(self.centralwidget)
+        self.back_btn.setObjectName("back_btn")
+        self.back_btn.setGeometry(QRect(720, 620, 80, 30))
+        self.back_btn.setText("Back")
+
 
         # The add Note button
         self.add_note_btn = QPushButton(self.centralwidget)
         self.add_note_btn.setGeometry(QRect(700, 81 , 170, 60))
         self.add_note_btn.setObjectName("add_note_btn")
-        self.add_note_btn.setText("Add notes")
-
-
-
-        # The notes table
-
-        note_table_width =  170
-        note_row_count = 5
-        self.notes_widget = QTableWidget(self.centralwidget)
-        self.notes_widget.setGeometry(QRect(700, 151, note_table_width, 261))
-        self.notes_widget.setColumnCount(1)  # Set the number of columns
-        self.notes_widget.setHorizontalHeaderLabels(["Column 1", "Column 2", "Column 3"])
-        self.notes_widget.setRowCount(note_row_count)  # Set the number of rows
-        self.notes_widget.setColumnWidth(0, note_table_width)
-        self.notes_widget.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.notes_widget.verticalHeader().setVisible(False)
-        self.notes_widget.horizontalHeader().setVisible(False)
-        
-        for row in range(5):
-            notes = QTableWidgetItem(f"Ingredient {row + 1}")
-            self.notes_widget.setItem(row, 0, notes)
-
-
- 
+        self.add_note_btn.setText("Add/view notes")
 
         # The header text
+
         self.text_label = QLabel(self.centralwidget)
         self.text_label.setGeometry(QRect(35, 10, 662, 51))
-        self.text_label.setText("Recipe")
-        
-        
+        font = QFont()
+        font.setPointSize(20)  # Set the font size to 14
+        font.setBold(True)  # Make the text bold
+        self.text_label.setFont(font)
+
         # set the colum width for the ingredient table
         table_width = 630
         ingredient_row_count = 6
         colum_wid = math.floor(table_width / 4) + 8
-        self.ingredient_widget = QTableWidget(self.centralwidget)
-        self.ingredient_widget.setGeometry(QRect(35, 80, table_width, 261))
-        self.ingredient_widget.setColumnCount(4)  # Set the number of columns
-        self.ingredient_widget.setRowCount(ingredient_row_count)  # Set the number of rows
-        self.ingredient_widget.setHorizontalHeaderLabels(["Ingredient Number", "Ingredient", "amount", "Unit"])
-        self.ingredient_widget.setColumnWidth(0, colum_wid)
-        self.ingredient_widget.setColumnWidth(1, colum_wid)
-        self.ingredient_widget.setColumnWidth(2, colum_wid)
-        self.ingredient_widget.verticalHeader().setVisible(False)
-        self.reading_recipe_ingredient(self.recipe_id)
-        self.populate_ingredient_table()
+        self.ingredient_table = QTableWidget(self.centralwidget)
+        self.ingredient_table.setGeometry(QRect(35, 80, table_width, 261))
+        self.ingredient_table.setColumnCount(4)
+        self.ingredient_table.setRowCount(ingredient_row_count)
+        self.ingredient_table.setHorizontalHeaderLabels(["Ingredient Number", "Ingredient", "amount", "Unit"])
+        self.ingredient_table.setColumnWidth(0, colum_wid)
+        self.ingredient_table.setColumnWidth(1, colum_wid)
+        self.ingredient_table.setColumnWidth(2, colum_wid)
+        self.ingredient_table.verticalHeader().setVisible(False)
+
 
 
 
@@ -127,33 +101,32 @@ class MainWindow(QMainWindow):
         self.step_table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.step_table.verticalHeader().setVisible(False)
         self.step_table.setColumnWidth(1, colum_wid)
-        self.reading_recipe_steps(self.recipe_id)
-        self.populate_steps_table()
-    
-
-        font = QFont()
-        font.setPointSize(20)  # Set the font size to 14
-        font.setBold(True)  # Make the text bold
-        self.text_label.setFont(font)
 
 
-    
 
 
-    def pushing_recipe_steps(self):
+
+        self.read_all_data()
+
+
+    def read_all_data(self):
         """
-        Adds an entry to the recipe_steps database
+        reads all the data and populate all the tables
+        """
+        self.reading_main(self.recipe_id)
+        self.reading_recipe_steps(self.recipe_id)
+        self.reading_recipe_ingredient(self.recipe_id)
+        self.text_label.setText(self.main_data[0][1])
+        self.populate_steps_table()
+        self.populate_ingredient_table()
+
+
+    def reading_main(self , recipe_id):
+        """
+        Reads all the data in recipe_steps and prints it out
         """
         conn = db.create_connection()
-        
-        curr_id = db.query_max_id(conn, "recipe_steps") + 1
-
-        task = (curr_id ,curr_id , "task")
-
-        db.create_task(conn, task, "recipe_steps")
-        
-        print("added")
-
+        self.main_data = db.db_query(conn, "main", "recipe_id", recipe_id)
 
     def reading_recipe_steps(self , recipe_id):
         """
@@ -161,7 +134,8 @@ class MainWindow(QMainWindow):
         """
         conn = db.create_connection()
         self.steps_data = db.db_query(conn, "recipe_steps", "recipe_id", recipe_id)
-        
+
+
     def populate_steps_table(self):
         """
         This read the db on recipe_steps and populate the corrisponding table with the data
@@ -189,27 +163,36 @@ class MainWindow(QMainWindow):
         for row in range(ingredient_row):
             ingredient_number = QTableWidgetItem(f"{row + 1}")
             ingredient_number.setTextAlignment(Qt.AlignCenter)
-            self.ingredient_widget.setItem(row, 0, ingredient_number)
-            
+            self.ingredient_table.setItem(row, 0, ingredient_number)
             ingredient_name = QTableWidgetItem(f"{self.ingredient_data[row][1]}")
-            self.ingredient_widget.setItem(row, 1, ingredient_name)
-            
+            self.ingredient_table.setItem(row, 1, ingredient_name)
             ingredient_amount = QTableWidgetItem(f"{self.ingredient_data[row][2]}")
-            self.ingredient_widget.setItem(row, 2, ingredient_amount)
-            
+            self.ingredient_table.setItem(row, 2, ingredient_amount)
             ingredient_unit = QTableWidgetItem(f"{self.ingredient_data[row][3]}")
-            self.ingredient_widget.setItem(row, 3, ingredient_unit)
+            self.ingredient_table.setItem(row, 3, ingredient_unit)
+
+    def clear_all_data(self):
+        """
+        clears all the data in preperation for another set of data 
+        """
+        self.step_table.clear()
+        self.ingredient_table.clear()
+        self.text_label.clear()
+
 
 class input_window(QMainWindow):
+    """
+    
+    """
     def __init__(self):
         super().__init__()
         self.setWindowTitle("input Window")
 
 
-        inputWindow_width = 650
-        inputWindow_height = 350
+        input_window_width = 650
+        input_window_height = 350
 
-        self.setGeometry(0, 0, inputWindow_width, inputWindow_height)
+        self.setGeometry(0, 0, input_window_width, input_window_height)
         self.centralwidget = QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
         self.setCentralWidget(self.centralwidget)
@@ -218,7 +201,6 @@ class input_window(QMainWindow):
 def main():
     """Runs the main function"""
 
-    
     app = QApplication(sys.argv)
     window = MainWindow()
 
