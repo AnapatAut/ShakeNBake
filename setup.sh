5e16f18b8618969de78414eb4e61df108b5c26ff
Aut <anapat5937@gmail.com>
2023-11-08 15:03:56 +0700

    setup file for linux to merge and deploy application

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rm -r build
+rm -r dist
+rm -r ui_control.spec
+pyinstaller --add-data "data/recipe_database.db3:data/recipe_database.db3" code/ui_control.py  code/recipe_creation.py  code/recipe_display.py code/main_menu.py code/history_note.py code/db_manager.py 
+cd dist
+mv ui_control/_internal/data/ ui_control/
+cd ui_control/data/
+ls
+mv recipe_database.db3/ ..
+cd ..
+mv recipe_database.db3/recipe_database.db3 data/
+rmdir recipe_database.db3
