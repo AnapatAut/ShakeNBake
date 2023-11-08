8fa3c3dedf01bc326127b0784f9313b94a5e92ce
Inkaphol(Ink) <inkaphol@gmail.com>
2023-11-08 12:11:25 +0700

    notes are now able to be added andremoved from the database. The history notes page now no longer crashes on start up

--- code/history_note.py
+++ code/history_note.py
@@ -1,29 +1,28 @@
 """historynoteui.py
 """
-#Python code for UI of historynote
+# Python code for UI of historynote
 
 # Created by Shine on 19 Oct 2023
 
-#Modified by Shine on 23 Oct 2023
+# Modified by Shine on 23 Oct 2023
 
-#Modified to be able to delete specific rows
+# Modified to be able to delete specific rows
 
-#Modified by Shine on 26 Oct 2023
+# Modified by Shine on 26 Oct 2023
 
-#Modified for history note text box size
-#Modified for limit text size input of note
-#Modified to add back button to go to recipe page
+# Modified for history note text box size
+# Modified for limit text size input of note
+# Modified to add back button to go to recipe page
 
 
-#Modified by Shine on 2 Nov 2023
+# Modified by Shine on 2 Nov 2023
 
-#Modified to directly view recipe without clicking view button
-#Modified to directly alert to 'add note' when clicking add button without data
-#Modified to improve size of history note table
+# Modified to directly view recipe without clicking view button
+# Modified to directly alert to 'add note' when clicking add button without data
+# Modified to improve size of history note table
 
-#Modified by Shine on 7 Nov 2023
-#Modified to do the connection with db_manager
-
+# Modified by Shine on 7 Nov 2023
+# Modified to do the connection with db_manager
 
 
 import sys
@@ -48,29 +47,27 @@ from PyQt5.QtCore import QRect, Qt
 import db_manager as db
 
 
-
 class App(QWidget):
     """History note window"""
+
     def __init__(self):
         super().__init__()
         self.title = 'Recipe History Notes'
         self.recipe_id = 1
         # self.recipe_list = [(1,'Sushi'),(2,'Fine')]
         self.init_ui()
-        
+
     def init_ui(self):
         """Set up the UI page"""
-        
+
         self.setWindowTitle(self.title)
         self.setGeometry(100, 100, 920, 680)
-        conn = db.create_connection()
 
-        name=db.db_query(conn, "main", "recipe_id", self.recipe_id)
+        name = [[0, "No Recipe Found"]]
         self.recipe_label = QLabel(
-        f'<html><head/><body><p><span style=" font-size:20pt; font-weight:600;">Recipe Name:  { name[0][1] }</span></p></body></html>'
+            f'<html><head/><body><p><span style=" font-size:20pt; font-weight:600;">Recipe Name:  {name[0][1]}</span></p></body></html>'
         )
         self.recipe_textbox = QLabel(self)
-
 
         # # self.recipe_textbox.setText("Sushi")
         # self.recipe_textbox.setText([recipe[1] for recipe in self.recipe_list if recipe[0] == self.recipe_id][0])
@@ -85,8 +82,8 @@ class App(QWidget):
         self.history_table.setRowCount(0)
         self.history_table.setColumnCount(2)
         self.history_table.setHorizontalHeaderLabels(["Timestamp", "History Note"])
-        self.history_table.setEditTriggers(QTableWidget.NoEditTriggers)  
-        self.history_table.setSelectionBehavior(QTableWidget.SelectRows)  
+        self.history_table.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.history_table.setSelectionBehavior(QTableWidget.SelectRows)
 
         self.add_button = QPushButton('Add', self)
         self.add_button.clicked.connect(self.add_clicked)
@@ -115,11 +112,11 @@ class App(QWidget):
         button_layout.addWidget(self.back_button)
 
         self.layout.addLayout(button_layout)
-        
+
         self.setLayout(self.layout)
 
-        self.show()  
-    
+        self.show()
+
     def add_clicked(self):
         """Add button to add history note"""
         # recipe_name = self.recipe_textbox.text()
@@ -133,7 +130,7 @@ class App(QWidget):
                 curr_id = db.query_max_id(conn, table) + 1
                 data_to_insert = [curr_id, self.recipe_id, note_details, timestamp]
                 db.create_task(conn, data_to_insert, table)
-                QMessageBox.about(self, "Success", "Note added successfully!")
+                self.write_history_textbox.clear()
                 self.view_history()
             else:
                 QMessageBox.about(
@@ -144,23 +141,22 @@ class App(QWidget):
         else:
             QMessageBox.about(self, "Error", "Note can't be blank. Please input some text.")
 
-
     def delete_clicked(self):
         """Delete button to delete history note"""
         current_row = self.history_table.currentRow()
-        if current_row >= 0:
-            history_id = self.history_table.item(current_row, 0).text()
-            conn = db.create_connection()
-            db.db_remove_history_notes(conn, history_id,self.recipe_id)
-            self.history_table.removeRow(current_row)
-            QMessageBox.about(self, "Success", "Note deleted successfully!")
+        conn = db.create_connection()
+        all_notes = db.db_query(conn, "history_note", "recipe_id", self.recipe_id)
 
+        if current_row >= 0:
+            conn = db.create_connection()
+            db.db_remove_history_notes(conn, all_notes[current_row][0], self.recipe_id)
+            self.history_table.removeRow(current_row)
 
     def clear_history_table(self):
         """Clear the history table"""
         self.history_table.setRowCount(0)
+        print(self.recipe_id)
 
-    
     def back_clicked(self):
         """Go back to the previous page"""
         print("Go back to recipe page")
@@ -168,22 +164,24 @@ class App(QWidget):
         self.close()
 
     def view_history(self):
-        """View history note"""
-        recipe_id = 1
         conn = db.create_connection()
-        notes=db.db_query(conn, "history_note", "recipe_id", recipe_id)
+        name = db.db_query(conn, "main", "recipe_id", self.recipe_id)
+        print(name, end="--------\n")
+        notes = db.db_query(conn, "history_note", "recipe_id", self.recipe_id)
 
         self.history_table.setRowCount(0)  # Clear the table before populating new data
+        self.recipe_label.setText(
+            f'<html><head/><body><p><span style=" font-size:20pt; font-weight:600;">Recipe Name:  {name[0][1]}</span></p></body></html>')
 
-        if not notes:
-            QMessageBox.about(self, "Info", "No history notes found for this recipe.")
-        else:
+        if notes:
             for row, (history_id, _, timestamp, note) in enumerate(notes):
                 self.history_table.insertRow(row)
                 self.history_table.setItem(row, 0, QTableWidgetItem(str(note)))
                 self.history_table.setItem(row, 1, QTableWidgetItem(timestamp))
                 # self.history_table.setItem(row, 2, QTableWidgetItem(note))
-        self.history_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)                
+        self.history_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
