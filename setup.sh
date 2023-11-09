6a31f3a046b692c5d0110385ba477ed544c2688f
Aut <anapat5937@gmail.com>
2023-11-09 16:03:19 +0700

    directory output after setup is more organized

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-rm -r build
-rm -r dist
-rm -r ui_control.spec
+pip install pyqt5
+rm -r ui_control/
 pyinstaller --collect-all code/ui_control.py --add-data "data/recipe_database.db3:data/recipe_database.db3" code/ui_control.py  code/recipe_creation.py  code/recipe_display.py code/main_menu.py code/history_note.py code/db_manager.py 
-cd dist
+rm -r build
+rm -r ui_control.spec
+mv dist/ui_control ui_control/
+rm -r dist
 mv ui_control/_internal/data/ ui_control/
 cd ui_control/data/
 mv recipe_database.db3/ ..
